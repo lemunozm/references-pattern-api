e1bfa46aa187af26e90fc0066910b3313490c8dd
Luis Enrique Muñoz Martín <lemunozm@gmail.com>
2019-11-08 21:15:08 +0100

    Added readme

--- README.md
+++ README.md
@@ -1,2 +1,12 @@
 # references-pattern-api
-Cpp header library that facilitates the creation of a reference pattern API.
+C++11 header-only that facilitates the creation of a reference pattern API.
+
+Given an interface class based on `shared_ptr` objects,
+this utility offer an easy way to create a layer that avoids the `shared_ptr` management across the API.
+In other words, transform an API as `std::shared_ptr<MyClass> myObj;` into `MyClass myObj;`
+keeping the behaviour (Now `MyClass` acts as a `shared_ptr<MyClass`).
+
+You can see a small example of this concept into the [example](example) folder.
+
+## How to use?
+Easy! copy and paste the utility header [`include/refapi/refapi.hpp`](include/refapi/refapi.hpp) into your project and include it.
--- include/refapi/refapi.hpp
+++ include/refapi/refapi.hpp
@@ -1,40 +1,6 @@
 #ifndef REFAPI__REFERENCE_HPP
 #define REFAPI__REFERENCE_HPP
 
-#include <memory>
-
-namespace refapi {
-
-template<typename IMPL>
-class Reference
-{
-public:
-    Reference(std::nullptr_t null)
-        : impl_(nullptr)
-    {}
-
-    Reference(const Reference& reference) = default;
-
-    Reference(const std::shared_ptr<IMPL>& impl)
-        : impl_(impl)
-    {}
-
-    bool operator == (const Reference& reference)
-    {
-        return impl_ == reference.impl_;
-    }
-
-protected:
-    std::shared_ptr<IMPL>& impl() { return impl_; }
-    const std::shared_ptr<IMPL>& impl() const { return impl_; }
-
-    void clear() { impl_.reset(); }
-
-private:
-    std::shared_ptr<IMPL> impl_;
-};
-
-
 #define REF_API_CLASS_BASE(API_CLASS, IMPL_CLASS) \
     protected: \
     API_CLASS(const std::shared_ptr<IMPL_CLASS>& other) \
@@ -51,10 +17,49 @@ private:
     { \
         return std::static_pointer_cast<IMPL_CLASS>(API_BASE_CLASS::impl()); \
     } \
+    \
     const std::shared_ptr<IMPL_CLASS> impl() const \
     { \
         return std::static_pointer_cast<IMPL_CLASS>(API_BASE_CLASS::impl()); \
     }
+
+#include <memory>
+
+namespace refapi {
+
+template<typename IMPL>
+class Reference
+{
+public:
+    Reference()
+        : impl_(nullptr)
+    {}
+
+    Reference(const Reference& reference) = default;
+
+    Reference(const std::shared_ptr<IMPL>& impl)
+        : impl_(impl)
+    {}
+
+    bool operator == (const Reference& reference)
+    {
+        return impl_ == reference.impl_;
+    }
+
+    operator bool() const
+    {
+        return impl_.get() != nullptr;
+    }
+
+protected:
+    std::shared_ptr<IMPL>& impl() { return impl_; }
+    const std::shared_ptr<IMPL>& impl() const { return impl_; }
+
+    void clear() { impl_.reset(); }
+
+private:
+    std::shared_ptr<IMPL> impl_;
+};
 
 } //namespace refapi
 
